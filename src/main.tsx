ef4b9b85fdcae936db62e686bfe234d9e7f19a92
EPAM\Janos_Bagoly <Janos_bagoly@epam.com>
2023-05-13 18:09:14 +0200

    chore: add project skeleton

---
+++ src/main.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Home from './pages/home/home';
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+    <Home />
+  </React.StrictMode>
+);

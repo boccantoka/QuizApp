e33222134e6af3fa1cd1bcb963afc2f19e526112
EPAM\Janos_Bagoly <Janos_bagoly@epam.com>
2023-05-14 21:15:52 +0200

    feat: add config for quiz api

---
+++ src/configs/quiz-config.ts
@@ -0,0 +1,7 @@
+import { QuestionDifficulty, QuestionType } from '../services/quiz';
+
+export const quizApiConfig = {
+  questionDifficulty: QuestionDifficulty.Hard,
+  questionType: QuestionType.Boolean,
+  questionNumber: 10,
+};

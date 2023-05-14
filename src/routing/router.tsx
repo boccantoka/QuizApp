5b6c5ae59ef5a04ec943a165dd41343b3c87e6d5
EPAM\Janos_Bagoly <Janos_bagoly@epam.com>
2023-05-14 17:01:47 +0200

    feat: add error handling to routes and loader for quiz route

--- src/routing/router.tsx
+++ src/routing/router.tsx
@@ -2,14 +2,45 @@ import { createBrowserRouter } from 'react-router-dom';
 import { HomePage } from '../pages/home';
 import { QuizPage } from '../pages/quiz';
 import { PagePaths } from './routes.constants';
+import { ErrorPage } from '../pages/error';
+import { RootLayout } from '../layouts';
+import {
+  QuestionDifficulty,
+  QuestionType,
+  fetchQuestions,
+} from '../services/quiz';
+
+export const questionLoader = async () => {
+  try {
+    return await fetchQuestions(
+      10,
+      QuestionDifficulty.Hard,
+      QuestionType.Boolean
+    );
+  } catch (error) {
+    throw new Error('Can not fetch questions');
+  }
+};
 
 export const router = createBrowserRouter([
   {
-    path: PagePaths.HomePage,
-    element: <HomePage />,
-  },
-  {
-    path: PagePaths.QuizPage,
-    element: <QuizPage />,
+    element: <RootLayout />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: PagePaths.HomePage,
+        element: <HomePage />,
+        errorElement: <ErrorPage />,
+      },
+      {
+        path: PagePaths.QuizPage,
+        element: <QuizPage />,
+        loader: questionLoader,
+      },
+      {
+        path: '/*',
+        element: <ErrorPage message="Page not found" />,
+      },
+    ],
   },
 ]);
